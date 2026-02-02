e01cd520e126c4c571a9822270278261b99713bd
Carry Chen <823212557@qq.com>
2026-02-02 22:24:12 +0800

    Initial Commit

---
+++ CNovalReaderTests/CNovalReaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  CNovalReaderTests.swift
+//  CNovalReaderTests
+//
+//  Created by 陈凯瑞 on 2026/2/2.
+//
+
+import Testing
+@testable import CNovalReader
+
+struct CNovalReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
